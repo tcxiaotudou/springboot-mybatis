51ff97a2cc7c2fba354458c2ace4e7fa911bfbce
tcxiaotudou <2290262044@qq.com>
2018-05-11 14:44:15 +0800

    idea git test

---
+++ src/main/java/com/codedou/springbootmybatis/SpringbootMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.codedou.springbootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisApplication.class, args);
+	}
+}
+
+
+
